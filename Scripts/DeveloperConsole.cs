bc896a71a50bee1b1c5300c62df6f72768177037
sliptrixx <hibnuhishath1@gmail.com>
2021-08-14 23:06:11 +0530

    Fixed an include error

--- Scripts/DeveloperConsole.cs
+++ Scripts/DeveloperConsole.cs
@@ -7,10 +7,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using UnityEditor;
+using UnityEngine;
 
 #if UNITY_EDITOR
-using UnityEngine;
+using UnityEditor;
 #endif
 
 namespace Hibzz.Console
